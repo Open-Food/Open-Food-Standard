ed5a04e3a0f42a96fdc8bff18fd3975f61680508
Kalvir Sandhu <kalv@kalv.co.uk>
2011-12-03 11:36:18 -0800

    Adding initial rails project to host the API

---
+++ api/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Api::Application.config.secret_token = 'ff8585b255fe76a58b1dbc608b72436de3b37e615354356a399be2ca483b173e488fe9d084362bf3b847e022a67734973a00568ea8e3e47f86000aa5a439e18c'
