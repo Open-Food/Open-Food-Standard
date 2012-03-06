fb63bf3064f3698e76a0fac4e74515e5d1f038e3
Chach Sikes <chachasikes@gmail.com>
2012-03-05 16:23:15 -0800

    Moving website to a different repo.

--- web/js/amplify-1.1.0/src/test/testswarm.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var url = window.location.search;
-	url = decodeURIComponent( url.slice( url.indexOf( "swarmURL=" ) + 9 ) );
-	if ( !url || url.indexOf( "http" ) !== 0 ) {
-		return;
-	}
-
-	document.write( "<script src='http://swarm.amplifyjs.com/js/inject.js?" + (new Date).getTime() + "'></script>" );
-}() );
