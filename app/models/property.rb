185638d74b4970793d9d0cb1e1a154f86aa86e79
Kalvir Sandhu <kalv@kalv.co.uk>
2012-03-08 13:05:08 -0800

    The property should not return the food id back in it's json

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,8 @@
 class Property < ActiveRecord::Base
   belongs_to :food
   validates_presence_of :food, :name, :value
+
+  def as_json(options)
+    super(:except => [:food_id])
+  end
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -13,6 +13,12 @@ describe Property do
     Property.new(@attr.merge(:food => nil)).should_not be_valid
   end
 
+  it 'should not return the food id in the json of a property' do
+    property = Property.create(@attr)
+    parsed_property = ActiveSupport::JSON.decode(property.to_json)
+    parsed_property["food_id"].should be_nil
+  end
+
   it 'should require a name and value' do
     [:name, :value].each do |attribute|
       Property.new(@attr.merge(attribute => nil)).should_not be_valid
